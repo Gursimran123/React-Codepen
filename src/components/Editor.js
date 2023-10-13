b478ed099fb0eb191bbe2d1cfae7cb4801c2b043
Gursimran Singh <gsb70720@gmail.com>
2023-10-13 22:09:23 +0530

    changes

---
+++ src/components/Editor.js
@@ -0,0 +1,78 @@
+import React,{useState} from 'react'
+import { Box, styled } from '@mui/material';
+import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
+import {Controlled as ControlledEditor} from 'react-codemirror2';
+import '../App.css';
+
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import "codemirror/mode/xml/xml";
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+
+const Container=styled(Box)`
+   flex-grow:1;
+   flex-basis:0;
+   display:flex;
+   flex-direction:column;
+   padding:0px 8px 8px;
+`
+
+const Heading=styled(Box)`
+   background:#1d1e22;
+   display:flex;
+   padding:9px 12px;
+`;
+
+const Header= styled(Box)`
+   display:flex;
+   justify-content:space-between;
+   align-items:center;
+   background:#060606;
+   color:#aaaeb6;
+   font-weight:700;
+`
+const Editor = ({heading,icon,color,value,onChange}) => {
+
+  const [open,setOpen] = useState(true);
+
+  const handleChange = (editor,data,value) =>{
+    onChange(value);
+  }
+
+  return (
+    <Container style={open?null: {flexGrow:0}}>
+        <Header>
+            <Heading>
+                <Box component="span"
+                  style={{
+                    background:color,
+                    height:20,
+                    width:20,
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'center',
+                    borderRadius:5,
+                    marginRight:5,
+                    paddingBottom:2,
+                    color:'#000'
+                  }}
+                >{icon}</Box>
+                {heading}
+            </Heading>
+            <CloseFullscreenIcon fontSize="small" onClick={()=>setOpen(prevState=> !prevState)}/>
+        </Header>
+        <ControlledEditor className='controlled-editor'
+           value={value}
+          onBeforeChange={handleChange}
+          options={{
+            theme:'material',
+            lineNumbers:true
+          }}
+        />
+    </Container>
+  )
+}
+
+export default Editor
